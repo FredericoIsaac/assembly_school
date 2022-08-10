8e3b09417d1f97929ae3c61f8c76d682318b5c74
Frederico <fredyisaac@gmail.com>
2022-08-10 08:59:03 +0100

    application 2 of code to done, doing tests now

---
+++ Code_II_app_II/Code_II_app_II/Dunky.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_II_app_II
+{
+    internal class Dunky: IAnimal
+    {
+        public void Talk()
+        {
+            Console.WriteLine("Innnn ooooooo");
+        }
+    }
+}
---
+++ Code_II_app_II/Code_II_app_II/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Code_II_app_II
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IAnimal[] animals = { new Dog(), new Cat(), new Dunky(), new Cow(), new Duck() };
+            while (true)
+            {
+                Console.WriteLine("Let's see what is the sound of the animal?");
+                Console.WriteLine("Choose one by the number:");
+                Console.WriteLine("1 - Dog\n2 - Cat\n3 - Dunky\n4 - Cow\n5 - Duck");
+                string choosedAnimal = Console.ReadLine();
+                int animal;
+                if (int.TryParse(choosedAnimal, out animal))
+                {
+                    animals[animal - 1].Talk();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please insert a corret value (1 to 5)");
+                }
+            }
+
+        }
+    }
+}
